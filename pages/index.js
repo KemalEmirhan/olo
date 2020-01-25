d0333bac0602e6f16788febb1c7a9efd36406835
Emirhan Kemal Kösem <emirhankemalkosem@gmail.com>
2020-01-25 17:28:45 +0300

    chore: commit lint configs

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,12 @@
 import React from 'react';
 import Head from 'next/head';
 
-import styled from 'styled-components';
-
-const Title = styled.h1`
-  font-size: 2rem;
-  color: #cacaca;
-  text-decoration: underline;
-`;
-
 const Home = () => (
   <div>
     <Head>
-      <title>Home</title>
+      <title>olo</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
-
-    <div className="hero">
-      <Title>Welcome to Next.js!</Title>
-      <p className="description">
-        To get started, edit <code>pages/index.js</code> and save to reload.
-      </p>
-    </div>
   </div>
 );
 
