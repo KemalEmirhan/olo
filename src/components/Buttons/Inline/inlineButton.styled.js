14e6e8009f312b22289e6d47349f25aaf30d123f
Emirhan Kemal Kösem <emirhankemalkosem@gmail.com>
2020-02-01 00:14:06 +0300

    feat: create stories for button components :books:

--- src/components/Buttons/Inline/inlineButton.styled.js
+++ src/components/Buttons/Inline/inlineButton.styled.js
@@ -1,16 +1,27 @@
 import styled from 'styled-components';
 
 const BtnInline = styled.button`
-  width: 10rem;
-  height: 4rem;
+  width: 100%;
+  height: 2.8rem;
   border: none;
   background-color: transparent;
-  border-block-end: 1px solid #cacaca;
-  color: #ddd;
+  border-block-end: 1px solid #ddd;
+  color: #111;
+  font-size: 1rem;
+  font-weight: 400;
+  letter-spacing: 1px;
+  outline: none;
+  cursor: pointer;
 
   display: flex;
   place-content: center;
   align-items: center;
+
+  transition: border-block-end 0.2s ease-in-out;
+
+  &:hover {
+    border-block-end: 1px solid #111;
+  }
 `;
 
 export default BtnInline;
--- src/components/Buttons/Secondary/secondaryButton.styled.js
+++ src/components/Buttons/Secondary/secondaryButton.styled.js
@@ -1,15 +1,38 @@
 import styled from 'styled-components';
 
 const BtnSecondary = styled.button`
-  width: 10rem;
-  height: 4rem;
-  border: 1px solid #fff;
-  border-radius: 1rem;
+  width: 100%;
+  height: 2.8rem;
+  border: 1px solid #111;
+  border-radius: 2rem;
   background-color: transparent;
-  color: #ddd;
+  color: #111;
+  font-size: 1rem;
+  font-weight: 400;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  outline: none;
+  cursor: pointer;
+
   display: flex;
   place-content: center;
   align-items: center;
+
+  transition: all 0.15s ease-in-out;
+
+  &:hover {
+    box-shadow: 0px 5px 2px 0px #ddd;
+  }
+
+  &:active {
+    transform: translateY(3px);
+    outline: none;
+  }
+
+  &:visited,
+  &:link {
+    outline: none;
+  }
 `;
 
 export default BtnSecondary;
