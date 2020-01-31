2700e979b20bdad0e3ff77c54bbc1100721dde27
Emirhan Kemal Kösem <emirhankemalkosem@gmail.com>
2020-01-31 22:53:27 +0300

    fix: project structure

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import HeadSection from '../src/components/shared/head';
-import DefaultButton from '../src/components/Buttons/Default/defaultButton';
 
 const Login = () => (
   <React.Fragment>
     <HeadSection title="Login/Register" />
     <h1>Login/Register Page</h1>
-    <DefaultButton>Default Button</DefaultButton>
   </React.Fragment>
 );
 
